ea557cd90d66f88c92d1ce957135a932f02e944e
Emanuel Calderon <ecalde11@jh.edu>
2022-02-23 15:58:16 -0500

    updates

--- PA1/ScratchPaper.py
+++ PA1/ScratchPaper.py
@@ -1,4 +1,6 @@
 from cmath import sqrt
+import json
+from re import X
 
 x0 = 1; y0 = 2; x1 = 2; y1 = 3; x2 = 1; y2 = 2; x3 = 2; y3 = 3
 
@@ -7,18 +9,24 @@ arrayy
 
 print(pointList[0])
 
+def pointLocation(x):
+    hey = x
+    return hey
 
 
 def distanceBetweenPoints(xi, yi, xj, yj):
     print(xi + yi + xj + yj)
     
     delta = sqrt((xi-xj)**2 + (yi-yj)**2)
+    pointLocation(xi)
 
-    return delta
+    return delta, yi
 
 result = distanceBetweenPoints(x0, y0, x1, y1)
 
+value = result[1]
 print(result)
-
+print(type(result))
+print(value)
 
 
