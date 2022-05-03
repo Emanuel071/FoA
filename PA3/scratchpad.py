46ae546a5dff340a66cebbc61fd52eae30deeaf0
Emanuel Calderon <ecalde11@jh.edu>
2022-05-03 10:28:00 -0400

    f

--- PA3/scratchpad.py
+++ PA3/scratchpad.py
@@ -1,35 +1,38 @@
 A ="101"
 B ="01"
-S = "10011011"
+S = "000010011011"
     
 m = len(A)            # A is down the rows, indexed by i, starting 1
 n = len(B)            # B goes across cols, indexed by j, starting 1
 s = len(S)
+
+if S.index(1) == 0:
+    S.replace
 
 total = m + n
 print(s)
 print(total)
 i = 0
 
-while s != total or i < 10:
-    A_new = A*2
-    total = len(A_new) + n
-    if total == s:
-        A = A_new
-        m = len(A) 
-        break
-    B_new = B*2
-    total = m + len(B_new)
-    if total == s:
-        B = B_new
-        n = len(B) 
-        break
-    if total > s:
-        break
-    print(i)
-    i += 1
+# while s != total or i < 10:
+#     A_new = A*2
+#     total = len(A_new) + n
+#     if total == s:
+#         A = A_new
+#         m = len(A) 
+#         break
+#     B_new = B*2
+#     total = m + len(B_new)
+#     if total == s:
+#         B = B_new
+#         n = len(B) 
+#         break
+#     if total > s:
+#         break
+#     print(i)
+#     i += 1
 
-print(A)
-print(m)
-print(B)
-print(n)
+# print(A)
+# print(m)
+# print(B)
+# print(n)
